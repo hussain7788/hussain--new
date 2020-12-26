3b65abacbaa465602b9d1689dace6b3c6a6ffe7b
hussain7788 <valli.h7788@gmail.com>
2020-12-26 12:23:40 +0530

    deleted

--- Project17/app17/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Employee(models.Model):
-    idno = models.IntegerField(unique=True)
-    name = models.CharField(max_length=50)
-    salary = models.FloatField()
